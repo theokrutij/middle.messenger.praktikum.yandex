ce8e2bfeffe9d8080eb5fba7055748fa9d002f71
Fedor Krutiy <theokrutij@yandex.ru>
2024-07-01 20:53:18 +0400

    fix express server to serve dist/index.html on all routes

--- express_server.js
+++ express_server.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 app.use(express.static("dist"));
 
 app.get(
-	"/", 
-	(req, res) => {}
+	"*", 
+	(req, res) => res.sendFile(path.join(__dirname, "dist", "index.html"))
 );
 app.listen(
 	3000,
