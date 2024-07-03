dd5121fdf3452c7fdc0040a3bcfe35d3d014c403
Fedor Krutiy <theokrutij@yandex.ru>
2024-07-03 15:46:32 +0400

    add user type definition

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,17 @@
 export type props = {
-	[key: string]: string | number | boolean | (() => void)
+	[key: string]: string | number | boolean | (() => void) | null
 }
 
 export type Template<T extends props> = (
 	props: T
 ) => [string, (() => void)?, (() => void)?];
+
+export type User = {
+	username: string,
+	first_name: string,
+	second_name: string,
+	display_name: string | null,
+	email: string,
+	phone: string,
+	avatar_url: string | null
+}
