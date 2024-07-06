a3b72a9ab478274a826090bcf2da82cd323cc4b9
Fedor Krutiy <theokrutij@yandex.ru>
2024-07-06 04:48:50 +0400

    add props to ChatCard component

--- src/components/ChatCard/ChatCard.tmpl.ts
+++ src/components/ChatCard/ChatCard.tmpl.ts
@@ -3,15 +3,35 @@ import { Template } from "../../types";
 import classes from "./ChatCard.module.css";
 
 
-export const ChatCard: Template = () => {
+type props = {
+	name: string,
+	lastMessage: {
+		author: string,
+		text: string,
+		datetime: string
+	},
+	unreadCount: number,
+}
+
+export const ChatCard: Template<props> = ({
+	name,
+	lastMessage,
+	unreadCount,
+}: props) => {
 	const template = 
 	`
 	<div class=${classes["chat-card"]}>
 		<div class=${classes.avatar}></div>
-		<h3 class=${classes.name}>Chat name</h3>
-		<p class=${classes.message}>Cicero: <span class=${classes.text}>Lorem ipsum dolor sit</span></p>
-		<span class=${classes.datetime}>Today, 12:00</span>
-		<div class=${classes["unread-count"]}>3</div>
+		<h3 class=${classes.name}>${name}</h3>
+		<p class=${classes.message}>
+			${lastMessage.author}: <span class=${classes.text}>${lastMessage.text}</span>
+		</p>
+		<span class=${classes.datetime}>${lastMessage.datetime}</span>
+		${
+			unreadCount > 0 
+			? `<div class=${classes['unread-count']}>${unreadCount}</div>`
+			: ""
+		}
 	</div>
 	`
 	;	
