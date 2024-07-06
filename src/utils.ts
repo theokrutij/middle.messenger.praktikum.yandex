75547857d1a1cc945cd7ae626675d7668384b0d6
Fedor Krutiy <theokrutij@yandex.ru>
2024-07-06 08:15:41 +0400

    remove onclick from modal layer when hiding

--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,22 @@
+type State = {
+	onHide: ((event: MouseEvent) => void) | null
+} 
+
+const state: State = {
+	onHide: null
+};
+
 export const showModal = (modalHTML: string, onLoad: () => void, onUnload: () => void) => {
 	const modalLayer = <HTMLDivElement>document.querySelector("#modal-layer");
+	state.onHide = (event: MouseEvent) => {
+		if (event.target === event.currentTarget) {
+			hideModal(onUnload);
+		}
+	};
 	modalLayer.addEventListener(
 		"click", 
-		(event) => {
-			if (event.target === event.currentTarget) {
-				hideModal(onUnload);
-			}
-	});
+		state.onHide
+	);
 	modalLayer.classList.remove("hidden");
 	modalLayer.innerHTML = modalHTML;
 	onLoad();
@@ -14,8 +24,11 @@ export const showModal = (modalHTML: string, onLoad: () => void, onUnload: () =>
 
 export const hideModal = (onUnload: () => void) => {
 	const modalLayer = <HTMLDivElement>document.querySelector("#modal-layer");
+	modalLayer.removeEventListener(
+		"click",
+		state.onHide as (event: MouseEvent) => void
+	);
 	modalLayer.classList.add("hidden");
 	onUnload();
 	modalLayer.innerHTML = "";
 };
-
