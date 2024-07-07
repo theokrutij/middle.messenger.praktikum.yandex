a91f823ff83c26e03fefd5f3d97f7cb0c94ceacc
Fedor Krutiy <theokrutij@yandex.ru>
2024-07-07 15:29:05 +0400

    add Form component

---
+++ src/components/Form/Form.tmpl.ts
@@ -0,0 +1,52 @@
+import { Block } from "../../modules/Block";
+import { DefaultProps } from "../../types";
+import { InputField } from "../InputField/InputField.tmpl";
+
+import classes from "./Form.module.css";
+
+
+export type ButtonProps = Omit<DefaultProps, "id"> & {
+	id: string,
+	events: {
+		"click": (event: MouseEvent) => unknown
+	}
+}
+
+
+
+type FormProps = DefaultProps & {
+	inputFields: InputField[],
+	controls: Block<ButtonProps>[],
+	events: {
+		"submit": (event: Event) => unknown
+	},
+}
+
+export class Form extends Block<FormProps> {
+	constructor(props: FormProps) {
+		super({...props, className: classes.form}, "form");
+	}
+
+	template() {
+		const template = 
+		`
+		<div class=${classes.fields}>
+		${this.props.inputFields.map((field) => `<div id=${field.id}></div>`).join("\n")}
+		</div>
+		<div class=${classes.controls}>
+			${this.props.controls.map((control) => `<div id=${control.id}></div>`).join("\n")}
+		</div>	
+		`;
+
+		return template;
+	}
+
+	render() {
+		const stubIdsToComponents = {};
+
+		this.props.inputFields.forEach((field) => Object.assign(stubIdsToComponents, {[field.id!]: field}));
+		this.props.controls.forEach((control) => Object.assign(stubIdsToComponents, {[control.id!]: control}));
+		return this.compile(this.template(), stubIdsToComponents);
+	}
+};
+
