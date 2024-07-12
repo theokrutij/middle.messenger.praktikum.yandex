311c95f5be16125651614a2ab2da801cde93aef8
Fedor Krutiy <theokrutij@yandex.ru>
2024-07-12 18:24:37 +0400

    add Router class

---
+++ src/modules/Router.ts
@@ -0,0 +1,107 @@
+import { DefaultProps } from "../types";
+import { Block } from "./Block";
+
+
+
+class Route<PropsType extends DefaultProps> {
+	private _block: Block<PropsType> | null = null;
+	private _blockClass: typeof Block<PropsType>;
+	private _props: PropsType;
+	private _pathname: string;
+
+	constructor(pathname: string, blockClass: typeof Block<PropsType>, props: PropsType) {
+		this._pathname = pathname;
+		this._blockClass = blockClass;
+		this._props = props;
+	}
+
+	match(path: string) {
+		return this._pathname === path;
+	}
+
+
+	render() {
+		if (this._block === null) {
+			this._block = new this._blockClass(this._props);
+		}
+
+		if (this._block.render === undefined) {
+			throw new Error(`Render not defined for block ${this._blockClass}`);
+		}
+
+		return this._block.render();
+	}
+
+	leave() {
+		if (this._block !== null) {
+			this._block.hide();
+		}
+	}
+};
+
+
+export class Router {
+	private _routes: Route<DefaultProps>[];
+	private _currentRoute: Route<DefaultProps> | null = null; 
+	private __instance: Router | null = null;
+	private _rootElementQuery: string;
+
+	constructor(rootElementQuery: string) {
+		if (this.__instance !== null) {
+			this._routes = this.__instance._routes;
+			this._rootElementQuery = rootElementQuery;
+
+			return this.__instance;
+		}
+
+		this._routes = [];
+		this._rootElementQuery = rootElementQuery;
+		
+		this.__instance = this;
+	}
+
+	use<PropsType extends DefaultProps>(pathname: string, blockClass: typeof Block<PropsType>, props: PropsType) {
+		const route = new Route<PropsType>(pathname, blockClass, props);
+
+		this._routes.push(route);
+
+		return this;
+	}
+
+	go(pathname: string) {
+		window.history.pushState(
+			{pathname: pathname},
+			"",
+			pathname
+		);
+
+		this._onRoute(pathname);
+	}
+
+	private _getRoute(pathname: string) {
+		return this._routes.find(route => route.match(pathname));
+	}
+
+	private _onRoute(pathname: string) {
+		const route = this._getRoute(pathname);
+
+		if (route === undefined) {
+			return;
+		}
+
+		if (this._currentRoute !== null) {
+			this._currentRoute.leave();
+		}
+
+		this._currentRoute = route;
+		this._renderDocFragment(route.render());
+	}
+
+	private _renderDocFragment(frag: DocumentFragment) {
+		const rootElement = <HTMLElement>document.querySelector(this._rootElementQuery);
+
+		rootElement.innerHTML = "";
+		rootElement.appendChild(frag);
+	}
+}
+
